7a9584612c2cde3314d5e09ade1f5910ae786287
Anton Jeran Ratnarajah <73730558+anton-jeran@users.noreply.github.com>
2024-08-06 21:13:56 -0700

    Update download_model.sh

--- download_model.sh
+++ download_model.sh
@@ -1,2 +1,3 @@
 gdown https://drive.google.com/uc?id=1CcF1c9i76-MVPJ-PGoBwvaVtOUBPD57y
+gdown https://drive.google.com/uc?id=1TrnXjR-vrCrub_RY6kdEBqA0D4hGiIp3
 mv checkpoint* exp/autoencoder/symAD_vctk_48000_hop300/
